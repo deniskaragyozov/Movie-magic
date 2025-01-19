f1a51e842b633749595e06f604622f4b55e9b26b
Denis Karagyozov <jsdk9926@gmail.com>
2025-01-19 11:06:37 +0200

    Add search by genre and year

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -9,6 +9,13 @@ export default {
             result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
         }
 
+        if (filter.genre){
+            result = result.filter(movie => movie.genre.toLowerCase().includes(filter.genre.toLowerCase()));
+        }
+
+        if (filter.year){
+            result = result.filter(movie => movie.year === filter.year);
+        }
         return result;
     },
     findOne(movieId){
