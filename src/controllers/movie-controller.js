c64f8843e109179f360267b51248121eaa88b0f5
Denis Karagyozov <jsdk9926@gmail.com>
2025-01-19 09:07:58 +0200

    Add details page with hardcoded data

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,9 +1,14 @@
 import {Router} from 'express';
+import movies from '../movies.js'; 
 
 const movieController = Router();
 
 movieController.get('/create', (req, res) => {
     res.render('create');
-})
+});
+
+movieController.get('/:movieId/details', (req, res) => {
+    res.render('details', {movies});
+});
 
 export default movieController;
