84eaa22bb7841b9c68c39b4411879c600b8962b5
Denis Karagyozov <jsdk9926@gmail.com>
2025-01-26 09:42:25 +0200

    Refactor search page and functionality

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -4,29 +4,30 @@ import { v4 as uuid } from 'uuid';
 
 export default {
     getAll(filter = {}){
-        //let result = await Movie.find({}).lean();
-//
-       // if (filter.search){
-       //     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-       // }
-//
-       // if (filter.genre){
-       //     result = result.filter(movie => movie.genre.toLowerCase().includes(filter.genre.toLowerCase()));
-       // }
-//
-       // if (filter.year){
-       //     result = result.filter(movie => movie.year === filter.year);
-       // }
-        return Movie.find({});
+        let query = Movie.find({});
+
+        if (filter.search){
+        //TODO: fix partial case insensitive search
+        query = query.where({title: filter.search});
+        }
+
+        if (filter.genre){
+        //TODO: fix partial case insensitive search
+        query = query.where({genre: filter.genre});
+        }
+
+        if (filter.year){
+            query = query.where({year: Number(filter.year)})
+        }
+        return query;
     },
     getOne(movieId){
-            
+
     const movie = Movie.findById(movieId);
 
     return movie;
     },
     create(movieData){
-        const newId = uuid();
 
         movies.push({
             id: newId,
