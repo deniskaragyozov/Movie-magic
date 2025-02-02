42ac4ecad6e4a43e6efddb74cb518c290b925c8b
Denis Karagyozov <jsdk9926@gmail.com>
2025-02-02 09:19:54 +0200

    Add logout functionality

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -33,4 +33,10 @@ authController.post('/login', async (req, res) => {
     }
 });
 
+authController.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+
+    res.redirect('/');
+})
+
 export default authController;
